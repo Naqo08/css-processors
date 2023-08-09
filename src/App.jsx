d6127ffe6bc8e3e9aefa876fcdfa040a5f6880b8
Naqiuddin Kamil <naqiu923@gmail.com>
2023-08-09 15:04:16 +0800

    add sass

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='wrapper'>
+      <p className='title'>Hello React</p>
+    </div>
+    
+    
+    </>
+  );
+  
+}
+
+export default App
